d02f4b1e728d1d8147582f2f08d424f9ed099ca2
Heloisa <heloisagoncalves26@gmail.com>
2025-05-01 16:12:56 -0300

    Add nova solução para o problema Peça perdida OBI 2007

--- logic-challenges/Perdida.java
+++ logic-challenges/Perdida.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Perdida {
+    // Gerando um vetor para comparação
     public static int[] GerarTodasPecas(int n){
         // Gera um vetor com todas as peças de 1 até n
         int[] todasPecas = new int[n];
@@ -11,6 +12,7 @@ public class Perdida {
         return todasPecas;
     }
 
+    // Transformando a entrada em instância
     public static int[] ObterPecas(String vetor, int n){
         // Transforma a String recebida em um vetor de números
         String[] vetorSeparado = vetor.split(" ");
@@ -23,8 +25,9 @@ public class Perdida {
 
     }
 
+    // Algoritmo 1 -> Compara cada elemento de a com cada elemento de b até verificar o que tem em a que não está em b
+    // T(n) = O(n^2)
     public static int ObterFaltante(int[] a, int[] b, int n){
-        // Compara cada elemento de a com cada elemento de b até verificar o que tem em a que não está em b
         for (int i = 0; i < n; i++){
             boolean encontrado = false;
             for (int j = 0; j < n-1; j++){
@@ -43,6 +46,19 @@ public class Perdida {
 
     }
 
+    // Algoritmo 2 - Soma os números da entrada e do vetor para comparação, subtrai um do outro para obter a peça faltante
+    // T(n) = O(n)
+    public static int DiferencaVetores(int[] a, int[] b){
+        int somaA = 0;
+        int somaB = 0;
+        for (int i = 0; i < a.length; i++) {
+            somaA += a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            somaB += b[i];
+        }
+        return somaA - somaB;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Entrada
@@ -57,8 +73,10 @@ public class Perdida {
         int faltante = ObterFaltante(a, b, n);
 
         // Saida = elemento faltante
+        System.out.println("Algoritmo 1:");
         System.out.println(faltante);
-        
+        System.out.println("Algoritmo 2:");
+        System.out.println(DiferencaVetores(a, b));
 
         scanner.close();
     }
